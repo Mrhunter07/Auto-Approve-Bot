09a666cb2b41a0a19ec4bc091d3f7c5f13ff39bb
Mrhunter07 <163120495+Mrhunter07@users.noreply.github.com>
2024-07-24 10:13:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "your api id"))
-    API_HASH = getenv("API_HASH", "your api hash")
-    BOT_TOKEN = getenv("BOT_TOKEN", "your bot token")
-    FSUB = getenv("FSUB", "your channel username")
-    CHID = int(getenv("CHID", "your channel id"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb url")
+    API_ID = int(getenv("API_ID", "24314601"))
+    API_HASH = getenv("API_HASH", "ede341e2d490a0fad5469866dedf8a95")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7368957242:AAHC46iuv8l1x_aFy-HdDZO8vkbfEW-7g-w")
+    FSUB = getenv("FSUB", "CinepleX1")
+    CHID = int(getenv("CHID", "1001877433573"))
+    SUDO = list(map(int, getenv("SUDO", "640617767").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://22duddududdu:22duddududdu@cluster0.acjczor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
 cfg = Config()
